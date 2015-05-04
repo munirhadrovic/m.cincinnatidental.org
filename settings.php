d2f474fa6224ab0320271724cc55baa93a8c08da
Emir <emiratesbp@gmail.com>
2015-05-04 10:47:32 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="9200 Montgomery Rd Building G | Suite 21A"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Cincinnati, OH 45242";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=5139843443;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="slideup"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
